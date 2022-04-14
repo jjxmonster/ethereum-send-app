63e6a4d4d2a79086b7674db41abbb36a891c064b
jjxmonster <kuba.tarabasz@wp.pl>
2022-04-14 22:51:06 +0200

    address and amount state, check is address valid

--- assets/payment.ts
+++ assets/payment.ts
@@ -1,17 +1,23 @@
 import Web3 from 'web3';
 
-export const sendEthereum = async (setError: (err: any) => void) => {
+export const sendEthereum = async (
+   setError: (err: any) => void,
+   address: string,
+   amount: number
+) => {
    try {
       if (window.ethereum) {
+         // login with metamask wallet
          await window.ethereum.send('eth_requestAccounts');
          const web3 = new Web3(window.ethereum);
          const accounts = await web3.eth.getAccounts();
          const account = accounts[0];
-         // web3.eth.sendTransaction({
-         //    from: account,
-         //    to: '0x1ff4B89B68ACD98B890b9a829948CDc21B2B4634',
-         //    value: Web3.utils.toWei('1', 'ether'),
-         // });
+         // if user has logged in, perform the transaction
+         web3.eth.sendTransaction({
+            from: account,
+            to: address,
+            value: Web3.utils.toWei(amount.toString(), 'ether'),
+         });
       } else {
          setError({ message: 'No crypto wallet. Install it' });
       }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Web3 from 'web3';
 import type { NextPage } from 'next';
 
 import { sendEthereum } from '../assets/payment';
@@ -11,12 +12,23 @@ type ErrorState = {
 
 const Home: NextPage = () => {
    const [error, setError] = useState<ErrorState>();
+   const [recipientAddress, setRecipientAddress] = useState<string>('');
+   const [amount, setAmount] = useState<number>(0);
+
+   const handleSendEthereum = (): void => {
+      if (Web3.utils.isAddress(recipientAddress)) {
+         //  sendEthereum(setError, recipientAddress, amount);
+      } else {
+         setError({ message: 'Something is wrong with recipient address' });
+      }
+   };
+
    return (
       <div className='w-screen h-screen flex items-center justify-center bg-blue-700 flex-col'>
          <h1 className='text-white'>SEND ETHEREUM</h1>
          <div className='h-96 w-96 bg-blue-700 bg-white'>
             <button
-               onClick={() => sendEthereum(setError)}
+               onClick={handleSendEthereum}
                className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'
             >
                send

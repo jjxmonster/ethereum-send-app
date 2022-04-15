64da318cd1c1ecfcf8cf1bae34ffc48857bfeddb
jjxmonster <kuba.tarabasz@wp.pl>
2022-04-15 11:58:33 +0200

    send ethereum function, better ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,8 @@ const Home: NextPage = () => {
 
    const handleSendEthereum = (): void => {
       if (Web3.utils.isAddress(recipientAddress)) {
-         //  sendEthereum(setError, recipientAddress, amount);
+         setError({ message: '' });
+         sendEthereum(setError, recipientAddress, amount);
       } else {
          setError({ message: 'Something is wrong with recipient address' });
       }
@@ -25,15 +26,45 @@ const Home: NextPage = () => {
 
    return (
       <div className='w-screen h-screen flex items-center justify-center bg-blue-700 flex-col'>
-         <h1 className='text-white'>SEND ETHEREUM</h1>
-         <div className='h-96 w-96 bg-blue-700 bg-white'>
-            <button
-               onClick={handleSendEthereum}
-               className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'
-            >
-               send
-            </button>
-            {error?.message}
+         <div className='w-full max-w-md'>
+            <form className='bg-white shadow-md rounded px-5 pt-8 pb-10 mb-4'>
+               <div className='mb-4'>
+                  <label className='block text-gray-700 text-sm font-bold mb-2'>
+                     Recipient Address
+                  </label>
+                  <input
+                     onChange={e => setRecipientAddress(e.target.value)}
+                     className='shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                     id='address'
+                     type='text'
+                     placeholder='Address'
+                  />
+               </div>
+               <div className='mb-6'>
+                  <label className='block text-gray-700 text-sm font-bold mb-2'>
+                     Ethereum Amount
+                  </label>
+                  <input
+                     onChange={e => setAmount(Number(e.target.value))}
+                     className='shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
+                     id='amount'
+                     type='number'
+                     placeholder='Amount'
+                  />
+                  <p className='text-red-500 text-xs italic'>
+                     {error?.message}
+                  </p>
+               </div>
+               <div className='flex items-center justify-between'>
+                  <button
+                     onClick={handleSendEthereum}
+                     className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+                     type='button'
+                  >
+                     Send
+                  </button>
+               </div>
+            </form>
          </div>
       </div>
    );
